6742b13fb697e50ec35d3f363e68e5f428840b53
Jan Tojnar <jtojnar@gmail.com>
2018-12-09 03:09:39 +0100

    Try to export NotifyPage

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -3,6 +3,8 @@ import addNotifyPane from './addNotifyPane';
 import app from 'flarum/app';
 import saveSettings from 'flarum/utils/saveSettings';
 
-app.initializers.add('flarum-notify', app => {
+export { default as NotifyPage } from './components/NotifyPage';
+
+app.initializers.add('manelizzard-notify', app => {
 	addNotifyPane();
 });
